72ad70c38bf686469aa25a3900297a14e23a6462
Vivek Sabbani <sabbanivivekpro@gmail.com>
2020-03-16 16:13:34 +0530

    First Commit

---
+++ Dom.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector("button");
+var body = document.querySelector("body");
+btn.addEventListener("click",function(){
+    body.style.background = "Purple";
+
+});
---
+++ testing.js
@@ -0,0 +1,4 @@
+var btn = document.querySelector("button");
+btn.addEventListener("mouseenter",function(){
+    btn.classList.toggle("button-hover");
+});
